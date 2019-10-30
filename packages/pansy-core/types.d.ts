7fab297f17d0f7a74a4ae1214e1de2436d2ff8cf
wangxingkang <wangxingkang@sensoro.com>
2019-10-30 21:04:25 +0800

    feat: 补充提交

--- packages/pansy-core/types.d.ts
+++ packages/pansy-core/types.d.ts
@@ -1,22 +1,22 @@
-declare module '*.json'
+declare module '*.json';
 
 declare module 'stringify-author' {
-  type Stringify = (author: any) => string
+  type Stringify = (author: any) => string;
   const stringify: Stringify;
-  export = stringify
+  export = stringify;
 }
 
 declare module 'text-table' {
   interface Options {
-    stringLength?(str: string): number
+    stringLength?(str: string): number;
   }
-  type Table = (rows: Array<Array<string>>, opts?: Options) => string
+  type Table = (rows: Array<Array<string>>, opts?: Options) => string;
   const table: Table;
-  export = table
+  export = table;
 }
 
 declare module 'resolve';
 
-declare module 'rollup-plugin-babel'
+declare module 'rollup-plugin-babel';
 
-declare module 'babel-plugin-alter-object-assign'
+declare module 'babel-plugin-alter-object-assign';
