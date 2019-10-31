292b60ef50540c4becc101951749a34e0bf8371d
wangxingkang <wangxingkang@sensoro.com>
2019-10-31 13:49:37 +0800

    chore: upgrade string-width

--- types.d.ts
+++ types.d.ts
@@ -1,21 +1,21 @@
-declare module '*.json'
+declare module '*.json';
 
-declare module 'rollup-plugin-babel'
+declare module 'rollup-plugin-babel';
 
-declare module 'babel-plugin-alter-object-assign'
+declare module 'babel-plugin-alter-object-assign';
 
 declare module 'text-table' {
   interface Options {
-    stringLength?(str: string): number
+    stringLength?(str: string): number;
   }
-  type Table = (rows: Array<Array<string>>, opts?: Options) => string
-  const table: Table
-  export = table
+  type Table = (rows: Array<Array<string>>, opts?: Options) => string;
+  const table: Table;
+  export = table;
 }
 
-declare module 'resolve'
+declare module 'resolve';
 declare module 'stringify-author' {
-  type Stringify = (author: any) => string
-  const stringify: Stringify
-  export = stringify
+  type Stringify = (author: any) => string;
+  const stringify: Stringify;
+  export = stringify;
 }
