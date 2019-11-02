a201916095dba1cd7b70d46a4e0095756c30303f
wangxingkang <156148958@qq.com>
2019-11-02 12:14:31 +0800

    feat: 补充提交

--- examples/example-typescript/src/index.ts
+++ examples/example-typescript/src/index.ts
@@ -1,10 +1,10 @@
 interface Person {
-  firstName: string
-  lastName: string
+  firstName: string;
+  lastName: string;
 }
 
 function greeter(person: Person) {
-  return 'Hello, ' + person.firstName + ' ' + person.lastName
+  return 'Hello, ' + person.firstName + ' ' + person.lastName;
 }
 
 let user = { firstName: 'Jane', lastName: 'User' };
