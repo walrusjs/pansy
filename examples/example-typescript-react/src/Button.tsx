4267f6bf459090605c86af4c945fe8701d62889b
wangxingkang <156148958@qq.com>
2019-11-12 23:13:09 +0800

    feat: 完善示例

---
+++ examples/example-typescript-react/src/Button.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+interface ButtonProps {
+  text?: string
+}
+
+class Button extends React.Component<ButtonProps> {
+  static defaultProps = {
+    text: 'hello world'
+  }
+  render() {
+    const { text } = this.props;
+    return (
+      <button>
+        {text}
+      </button>
+    )
+  }
+}
+
+export default Button;
